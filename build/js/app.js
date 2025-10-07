e5864c31c446ba2ad430567d91e804c95364dd10
eduar <eduar9807mail.com>
2025-10-07 01:51:33 -0600

    agregando modal y funcionalidad de botones

--- build/js/app.js
+++ build/js/app.js
@@ -1,9 +1,40 @@
 let todosLosVideojuegos=[];
+let todasLasNoticias=[];
+
+
+  // Hero dinámico con autoplay y clic en cards
+  const hero = document.querySelector(".hero");
+  const title = document.getElementById("hero-title");
+  const desc = document.getElementById("hero-desc");
+  const cards = document.querySelectorAll(".card");
+
+  let index = 0;
+  const changeHero = (card) => {
+    hero.style.backgroundImage = `url(${card.dataset.bg})`;
+    title.textContent = card.dataset.title;
+    desc.textContent = card.dataset.desc;
+  };
+
+  // Cambiar con clic
+  cards.forEach((card, i) => {
+    card.addEventListener("click", () => {
+      index = i;
+      changeHero(card);
+    });
+  });
+
+  // Autoplay cada 5s
+  setInterval(() => {
+    index = (index + 1) % cards.length;
+    changeHero(cards[index]);
+  }, 5000);
+
+  // Inicial
+  changeHero(cards[0]);
 
 document.addEventListener('DOMContentLoaded', function(){
-    console.log('hola');
     obtenerJson();
-    console.log('hola');
+    obtenerJsonNoticias();
 })
 
 function obtenerJson(){
@@ -15,12 +46,20 @@ function obtenerJson(){
     .then(videojuegos=>{
         todosLosVideojuegos=videojuegos;
         mostrarVideojuegos(videojuegos, 'catalogoPrincipal');
+        //agregando eventos de botones
+        document.querySelectorAll('.btn-catalogo').forEach(btn =>{
+            btn.addEventListener('click', ()=>{
+                const categoria = btn.dataset.text;
+                filtrarVideojuegos(categoria);
+            });
+        });
     })
     .catch(error=> console.error('Error cargando el JSON de videojuegos', error))
 }
 
 function mostrarVideojuegos(videojuegos, contenedorId){
     const contenedor=document.getElementById(contenedorId);
+    contenedor.innerHTML = "";
     videojuegos.forEach( videojuegos=> {
         //Mezclamos videojuegos
         //const mezcladas = videojuegos.sort(()=> 0.5 - Math.random());
@@ -28,6 +67,65 @@ function mostrarVideojuegos(videojuegos, contenedorId){
         contenedor.appendChild(tarjetaVideojuego);
         
     });
+}
+
+function filtrarVideojuegos(categoria){
+    const contenedor = document.getElementById('catalogoPrincipal');
+    contenedor.innerHTML="";
+    let filtrados;
+    if(categoria=== "Todos"){
+        filtrados=todosLosVideojuegos;
+
+    }else{
+        //realiza un filtrado de videojuegos con un sub arreglo v
+        filtrados=todosLosVideojuegos.filter(v=> v.categoria === categoria);
+    }
+    mostrarVideojuegos(filtrados, 'catalogoPrincipal');
+}
+
+function crearModal(videojuegos, idmodal){
+    const contenedor_modal = document.getElementById(idmodal);
+    const overlay = document.getElementById('modal-cont');
+    contenedor_modal.innerHTML = "";
+    const contenedor_cierre = document.createElement('span');
+    contenedor_cierre.innerHTML=`
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-x-square-fill" viewBox="0 0 16 16">
+            <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708"/>
+        </svg>
+    `
+    contenedor_cierre.addEventListener('click', ()=>{
+        overlay.style.display="none";
+        contenedor_modal.innerHTML="";
+    });
+    //cerrar si damos clic fuera del modal
+    overlay.addEventListener('click', (e)=>{
+        if(e.target===overlay){
+            overlay.style.display="none";
+            contenedor_modal.innerHTML="";
+        }
+    })
+    const contenedor_info = document.createElement('div')
+    contenedor_info.classList.add('cont-modal');
+    contenedor_info.innerHTML= `
+        <div class="info-fila">
+            <div class="info-trailer">
+            <iframe src="${videojuegos.trailer}" allow="autoplay; encrypted-media" allowfullscreen frameborder="0"></iframe>
+            </div>
+            <div class="info-videojuego">
+            <h2>${videojuegos.nombre}</h2>
+            <p> Desarrolladora: ${videojuegos.Desarrolladora}</p>
+            <p> Categoria: ${videojuegos.categoria}</p>
+            <p> Clasificación: ${videojuegos.clasificacion}</p>
+            </div>
+        </div>
+        <div class="info-column">
+            <div class="info-sinopsis">
+                <p>${videojuegos.Sinopsis}</p>
+            </div>
+        </div>
+    `
+    contenedor_modal.append(contenedor_cierre);
+    contenedor_modal.append(contenedor_info);
 }
 
 function crearTarjetaCatalogo(videojuegos){
@@ -40,6 +138,7 @@ function crearTarjetaCatalogo(videojuegos){
     cont_img.classList.add('cont-image');
     cont_img.innerHTML=`
         <img src="${videojuegos.img}" alt=" ${videojuegos.alt}"/>
+        <img src="${videojuegos.img2}" />
     `;
     cont.appendChild(cont_img);
     const cont_info= document.createElement('div');
@@ -51,6 +150,13 @@ function crearTarjetaCatalogo(videojuegos){
         <p> $${videojuegos.Precio}</p>
     `;
     cont.appendChild(cont_info);
+    //Agregando evento clic para mostrar el modal
+    cont_img.addEventListener('click',()=>{
+        const modal = document.getElementById('modal-cont');
+        console.log("doy clicl")
+        modal.style.display='flex';
+        crearModal(videojuegos,'modal')
+    })
     const con_etiq= document.createElement('div');
     con_etiq.classList.add('cont-etiquetas');
     videojuegos.Plataformas.forEach(etq =>{
@@ -65,7 +171,7 @@ function crearTarjetaCatalogo(videojuegos){
 }
 
 
-let todasLasNoticias=[];
+
 
 function obtenerJsonNoticias(){
     fetch('data/noticias.json')
@@ -76,9 +182,10 @@ function obtenerJsonNoticias(){
 
         //Ordenamos las noticias por fecha (de más reciente a más antigua)
         noticias.sort((a, b) => new Date(b.fecha) - new Date(a.fecha));
+        console.log("ordene las noticias")
 
-    mostrarNoticiasPrincipales(noticias.slice(0, 2), 'noticias-grid'); // primeras 2 como principales
-    mostrarNoticiasMini(noticias.slice(2), 'noticiasminis-grid');
+    mostrarNoticiasPrincipales(noticias.slice(0, 2), 'cont-noticias-prin'); // primeras 2 como principales
+    mostrarNoticiasMini(noticias.slice(2), 'cont-noticias-mini');
     })
     .catch(error=> console.error('Error cargando el JSON de noticias', error))
 }
@@ -86,7 +193,7 @@ function obtenerJsonNoticias(){
 
 function mostrarNoticiasPrincipales(noticias, contenedorId){
     const contenedor=document.getElementById(contenedorId);
-    contenedor.innerHTML = ''; // Limpiar el contenedor antes de agregar nuevas noticias
+    //contenedor.innerHTML = ''; // Limpiar el contenedor antes de agregar nuevas noticias
     noticias.forEach( noticia=> {
         const article = document.createElement('article');
         article.classList.add('noticia');
@@ -124,8 +231,4 @@ noticias.forEach( noticia=> {
             </div>
             <p class="noticia-desc">${noticia.descripcion}</p>
             <a href="${noticia.link}" class="noticia-link">Más información</a>
-        </div>
-    `;
-    contenedor.appendChild(divmini);
-});
-}
+ 
